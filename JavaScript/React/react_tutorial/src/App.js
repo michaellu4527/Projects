7fedf9de19daa97834f3a76060ddfc7348b2cd92
Michael Lu <michael.lu4572@gmail.com>
2017-10-03 16:43:59 -0700

    Started react tutorial

--- JavaScript/React/react_tutorial/src/App.js
+++ JavaScript/React/react_tutorial/src/App.js
@@ -3,12 +3,34 @@ import Projects from './Components/Projects';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    // This needs to be called when you create a constructor
+    super();
+    this.state = {
+      projects: [
+        {
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'Ecommerce Shopping Cart',
+          category: 'Web Development'
+        },
+      ]
+    }
+  }
+
   render() {
     return (
       // Everything needs to be in ONE outer element. You CANNOT return 2 elements
+      // Below takes everything in the Projects component and outputs it (simple as that
       <div className="App">
         My App
-        <Projects />
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
--- JavaScript/React/react_tutorial/src/Components/Projectitem.js
+++ JavaScript/React/react_tutorial/src/Components/Projectitem.js
@@ -2,18 +2,11 @@ import React, { Component } from 'react';
 
 class Projects extends Component {
   render() {
-    let projectItems;
-    if (this.props.projects){
-      projectItems = this.props.projects.map(project => {
-        // console.log(project);
-      })
-    }
     return (
       // Everything needs to be in ONE outer element. You CANNOT return 2 elements
-      <div className="Projects">
+      <li className="Project">
         My Projects
-        {this.props.test}
-      </div>
+      </li>
     );
   }
 }
