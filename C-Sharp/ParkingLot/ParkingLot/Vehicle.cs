ad197cc0ae231bf90e4ff05cbe484d3fd9dc9362
Michael Lu <michael.lu4572@gmail.com>
2017-10-31 15:36:18 -0700

    Finished preliminary Vehicle class

--- C-Sharp/ParkingLot/ParkingLot/Vehicle.cs
+++ C-Sharp/ParkingLot/ParkingLot/Vehicle.cs
@@ -14,14 +14,29 @@ namespace ParkingLot
         protected int spotsNeeded;
         protected int size;
 
+        // Returns the size of the vehicle
         protected int getSize() {
             return size;
         }
 
+        // Will just retreive the number of spots needed to park vehicle.
         protected int getSpotsNeeded()
         {
             return spotsNeeded;
         }
 
+        // Will remove vehicle from spot
+        public void clearSpots()
+        {
+
+        }
+
+        // Will "park" a vehicle into a particular parking spot.
+        public void parkInSpot(ParkingSpot s)
+        {
+            parkingSpots.Add(s);
+        }
+
+        public abstract Boolean canFitInSpot(ParkingSpot spot);
     }
 }
