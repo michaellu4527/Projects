4e5fe285f8f3f585136006e3ba089884e4469234
Michael Lu <michael.lu4572@gmail.com>
2017-10-30 17:37:26 -0700

    Started parking lot project

---
+++ C-Sharp/ParkingLot/ParkingLot/ParkingSpot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParkingLot
+{
+    public class ParkingSpot
+    {
+        private Vehicle vehicle;
+        private VehicleSize spotSize;
+        private int row;
+        private int spotNumber;
+    }
+}
---
+++ C-Sharp/ParkingLot/ParkingLot/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParkingLot
+{
+    public enum VehicleSize { Motorcycle, Compact, Large }
+
+    public abstract class Vehicle
+    {
+        
+    }
+}
