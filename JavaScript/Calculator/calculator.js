06cc7423933cfc0e06d849a5985d60abbb3f53ef
Michael Lu <michael.lu4572@gmail.com>
2018-03-27 16:56:46 -0700

    Started initial JavaScript.

--- JavaScript/Calculator/calculator.js
+++ JavaScript/Calculator/calculator.js
@@ -1,6 +1,22 @@
-function squareNumber(num) {
-    var squaredNumber = num * num;
-    console.log("The result of squaring the number " + num + " is " + squaredNumber);
-    return squaredNumber;
-}
+/*jslint browser: true*/
+/*global $, jQuery*/
 
+$(document).ready(function () {
+    // Stores the inputs from the user to calculate later
+    var inputs = [""], totalString, operators1 = ["/", "*", "-", "+"];
+
+    var operators2 = ["."], nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    function getValue(input){
+        
+    }
+    
+    function update() {
+        totalString = inputs.join("");
+        $("#steps").html(totalString);
+    }
+    
+    function getTotal(){
+        
+    }
+});
