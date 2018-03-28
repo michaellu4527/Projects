d72867ecbd8a86df27d84de7eebe51e92112cc31
Michael Lu <michael.lu4572@gmail.com>
2018-03-27 18:23:57 -0700

    Numbers correctly show up on calculator now. Bug: They do not stay on screen after click event.

--- JavaScript/Calculator/calculator.js
+++ JavaScript/Calculator/calculator.js
@@ -1,22 +1,7 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
 
-$(document).ready(function () {
-    // Stores the inputs from the user to calculate later
-    var inputs = [""], totalString, operators1 = ["/", "*", "-", "+"];
-
-    var operators2 = ["."], nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    function getValue(input){
-        
-    }
-    
-    function update() {
-        totalString = inputs.join("");
-        $("#steps").html(totalString);
-    }
-    
-    function getTotal(){
-        
-    }
-});
+function numInput(val) {
+    "use strict";
+    document.getElementById("display").value += val;
+}
