c6c6dab4c9c339b1e5df43c019bada4f92a826fd
Michael Lu <michael.lu4572@gmail.com>
2018-03-28 12:33:22 -0700

    Added rest of JavaScript functions. Bug still needs to be resolved.

--- JavaScript/Calculator/calculator.js
+++ JavaScript/Calculator/calculator.js
@@ -1,7 +1,21 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
 
+function clearScreen(val) {
+    "use strict";
+    document.getElementById("display").value = val;
+}
+
 function numInput(val) {
     "use strict";
     document.getElementById("display").value += val;
+}
+
+function evaluate() {
+    "use strict";
+    try {
+        clearScreen(eval(document.getElementById("display").value));
+    } catch (e) {
+        clearScreen('Error');
+    }
 }
